6caf7f76c744a4367a048f65b37276032a23b81e
Colleen Messina <136169419+ColleenMessina@users.noreply.github.com>
2025-07-29 15:43:25 +0800

    Add files via upload

---
+++ Build a Record Collection (Objects)/BuildARecordCollection.js
@@ -0,0 +1,36 @@
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "") {
+    if (!("tracks" in records[id])) {
+      const emptyArr = [];
+      emptyArr.push(value);
+      records[id]["tracks"] = emptyArr;
+    } else {
+      records[id]["tracks"].push(value);
+    }
+  }
+  return records;
+}
